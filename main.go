29b8d576bf93675bb860fe64d35d09ae55810c06
stz184 <ivanov.bg@gmail.com>
2021-02-27 01:42:44 +0200

    Project init

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Gopher translator service!")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func main() {
+	handleRequests()
+}
